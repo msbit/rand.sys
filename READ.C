c0500367b1a95f23ce56d7f640920f1998de3f68
Tom <tom@msbit.com.au>
2022-10-08 12:11:48 +1100

    Add reader for arbitrary file

---
+++ READ.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  FILE *f;
+  int i;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s <file> <count>\n", argv[0]);
+    return -1;
+  }
+
+  f = fopen(argv[1], "rb");
+  if (f == NULL) {
+    perror("fopen");
+    return -1;
+  }
+
+  for (i = 0; i < atoi(argv[2]); i++) {
+    printf("%02x\n", fgetc(f));
+  }
+
+  fclose(f);
+
+  return 0;
+}
